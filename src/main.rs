bee8bfdeb5ce2caf90ee846de92097f6dc863d73
uncatched <litvinskyi.dev@gmail.com>
2023-03-10 19:23:22 +0200

    Add `message` field to `Config`

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,13 @@ pub struct Config {
     pub bundle_id: String,
     pub device_token: String,
     pub team_id: String,
-    pub key_path: String
+    pub key_path: String,
+    pub message: String
 }
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 5 {
+        if args.len() < 6 {
             return Err("Not enough arguments.")
         }
 
@@ -28,13 +29,15 @@ impl Config {
         let device_token = args[2].clone();
         let team_id = args[3].clone();
         let key_path = args[4].clone();
+        let message = args[5].clone();
 
         Ok(
             Config { 
                 bundle_id, 
                 device_token, 
                 team_id, 
-                key_path 
+                key_path,
+                message
             }
         )
     }
